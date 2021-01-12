08910cfdf177582f6851560b92be81c86ec775fb
Alex Rodriguez Just <alexjustdata@gmail.com>
2021-01-12 08:02:33 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Intro to Data Science with Python',
+    author='Alex Rodriguez Just',
+    license='',
+)
